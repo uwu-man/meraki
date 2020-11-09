5f8224e895ac4c116f064ffcd9c963736b076e74
Gabriel Lobo <gabriellobodurante@gmail.com>
2020-11-09 15:37:30 -0300

    componentização do sidenav-dashboard

---
+++ pages/dashboard/sidenav.js
@@ -0,0 +1,49 @@
+let renderSidenav = async (template, pageSelection) => {
+    let HTMLTemplate = await template.fetchTemplateHTML("./dashboard.sidenav.html");
+    switch (pageSelection){
+        case "inicio":
+            HTMLTemplate = HTMLTemplate
+                .replace('@{inicio-selection}',"class='side-nav-selected'")
+                .replace('@{dados-selection}',"")
+                .replace('@{material-selection}',"")
+                .replace('@{exercicios-selection}',"")
+                .replace('@{avisos-selection}',"");
+            break;
+        case "dados":
+            HTMLTemplate = HTMLTemplate
+                .replace('@{inicio-selection}',"")
+                .replace('@{dados-selection}',"class='side-nav-selected'")
+                .replace('@{material-selection}',"")
+                .replace('@{exercicios-selection}',"")
+                .replace('@{avisos-selection}',"");
+            break;
+        case "material":
+            HTMLTemplate = HTMLTemplate
+                .replace('@{inicio-selection}',"")
+                .replace('@{dados-selection}',"")
+                .replace('@{material-selection}',"class='side-nav-selected'")
+                .replace('@{exercicios-selection}',"")
+                .replace('@{avisos-selection}',"");
+            break;
+        case "exercicios":
+            HTMLTemplate = HTMLTemplate
+                .replace('@{inicio-selection}',"")
+                .replace('@{dados-selection}',"")
+                .replace('@{material-selection}',"")
+                .replace('@{exercicios-selection}',"class='side-nav-selected'")
+                .replace('@{avisos-selection}',"");
+            break;
+        case "avisos":
+            HTMLTemplate = HTMLTemplate
+                .replace('@{inicio-selection}',"")
+                .replace('@{dados-selection}',"")
+                .replace('@{material-selection}',"")
+                .replace('@{exercicios-selection}',"")
+                .replace('@{avisos-selection}',"class='side-nav-selected'");
+            break;
+        default:
+            break;
+            
+    }
+    document.getElementById('sidenav').innerHTML = HTMLTemplate;
+}
